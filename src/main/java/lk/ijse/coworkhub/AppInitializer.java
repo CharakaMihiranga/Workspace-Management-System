337a83437dcac5b2ffb259771c4c32c12cc831c0
Charaka <charakavmihiranga@gmail.com>
2023-12-27 21:16:19 +0530

    Architecture

---
+++ src/main/java/lk/ijse/coworkhub/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.coworkhub;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) { launch(args); }
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println( "Start" );
+    }
+}
