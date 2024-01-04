15662d2b7e10159b488c217291514135165c0091
CharakaMihiranga <charakavmihiranga@gmail.com>
2024-01-04 18:31:45 +0530

    image validation

--- src/main/java/lk/ijse/coworkhub/controller/ManageMembersFormController.java
+++ src/main/java/lk/ijse/coworkhub/controller/ManageMembersFormController.java
@@ -3,6 +3,7 @@ package lk.ijse.coworkhub.controller;
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -27,20 +28,38 @@ public class ManageMembersFormController {
     private AnchorPane paneMember;
     @FXML
     private ImageView imgMemberPhoto;
-    private JFileChooser fileChooser = new JFileChooser();
+    private FileChooser fileChooser = new FileChooser();
     private File file;
     private Image image;
 
     @FXML
     private void BrowseMemberPicture(ActionEvent actionEvent) throws IOException {
 
-        fileChooser.showOpenDialog(null);
-         file = fileChooser.getSelectedFile();
-        String path = file.getAbsolutePath();
-         image = new Image(path);
+        fileChooser.setTitle("Choose Image File");
+        file = fileChooser.showOpenDialog(null);
 
-        imgMemberPhoto.setImage(image);
+       try{
+           if (isImageFile(file)) {
+               String path = file.getAbsolutePath();
+               image = new Image("file:" + path);
+               imgMemberPhoto.setImage(image);
+           } else {
+               new Alert(Alert.AlertType.WARNING,"Invalid file type,Please choose a valid image file").showAndWait();
+           }
+       }catch (Exception e){
+           new Alert(Alert.AlertType.ERROR, "Unable to Select the Image! + ( "+ e + ")").showAndWait();
+       }
+    }
 
+    private boolean isImageFile(File file) {
+        if (file != null) {
+            String fileName = file.getName();
+            String extension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
 
+            return extension.equals("jpg") || extension.equals("jpeg") ||
+                    extension.equals("png") || extension.equals("gif") ||
+                    extension.equals("bmp") || extension.equals("tiff");
+        }
+        return false;
     }
 }
